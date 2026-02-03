5bee5dce5ff240ef985ca0b39d9a68aea0eac9f6
Tony Nguyen <tonngu137@gmail.com>
2026-02-03 06:41:07 +0100

    feat: add new helper functions

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,6 +1,22 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
+export function select(selector, scope = document) {
+	return scope.querySelector(selector);
+}
 
-export function dirname(metaUrl) {
-	return path.dirname(fileURLToPath(metaUrl));
+export function toArray(input, scope = document) {
+	if (typeof input === 'string') {
+		const elements = Array.from(scope.querySelectorAll(input));
+		return elements.length ? elements : false;
+	}
+
+	return Array.from(input);
+}
+
+export function wrap(min, max, index) {
+	// Handle array input
+	if (Array.isArray(min)) {
+		return this.wrapArray(min, max);
+	}
+
+	// Handle numeric range input
+	return this.wrapRange(min, max, index);
 }
