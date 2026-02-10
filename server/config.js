499d22f965070ca1cef5e8ac0dc0865986d70fb3
Tony Nguyen <tonngu137@gmail.com>
2026-02-10 09:18:23 +0100

    fix: replace ?? with ||

--- server/config.js
+++ server/config.js
@@ -4,7 +4,12 @@ dotenv.config();
 
 const config = {
 	env: process.env.NODE_ENV ?? 'development',
-	port: Number(process.env.PORT ?? 3000)
+	port: Number(process.env.PORT || 3000),
+	user: process.env.DB_USER ?? 'postgres',
+	host: process.env.DB_HOST ?? 'localhost',
+	database: process.env.DB_NAME ?? 'sportmeet_db',
+	password: process.env.DB_PASSWORD,
+	dbPort: Number(process.env.DB_PORT ?? 5432)
 };
 
 export default config;
